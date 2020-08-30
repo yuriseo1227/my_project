1e31c51b56a5caaffebe7a2c8c0688f212e6d601
yuri seo <syeuol@gmail.com>
2020-08-30 19:39:53 +0900

    (3)질문지 선택에 따른 결과지 띄우기
    -내용 전체 수정
    -if 함수로 질문 선택에 따라 결과 나오게끔 구동
    -결과1이 떠야하는 조건에서 결과3이 나오는 오류가 생김
    -결과 화면에서 첫화면으로 돌아가는 함수를 다양하게 시도했지만 리셋 실패

---
+++ app.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
