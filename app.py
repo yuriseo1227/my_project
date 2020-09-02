ee55070dbc0b9485ab303e35c597f9fed7366a06
yuri seo <syeuol@gmail.com>
2020-09-02 20:12:58 +0900

    마무리

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://sparta-yuri:sparta123!@13.125.210.185',27017)
 db = client.dbsparta
 
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
 
 # HTML 화면 보여주기
 @app.route('/')
